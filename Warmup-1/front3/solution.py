8f4c943da3447ae9bcd7134f0b6b85f8acc6c6a4
FarzanRashid <54062224+FarzanRashid@users.noreply.github.com>
2019-09-22 13:54:47 +0600

    Add solution for front3

--- Warmup-1/front3/solution.py
+++ Warmup-1/front3/solution.py
@@ -1,5 +1,5 @@
 def front3(str):
-    if len(str) < 3:
-        return str * 3
-    else:
-        return str[0:3] * 3
+     if len(str) < 3:
+         return str * 3
+     else:
+         return str[0:3] * 3
